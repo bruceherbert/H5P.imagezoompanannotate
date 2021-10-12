db5a5cfab20304f94e153d9d39ab9e1f81e512c6
bruceherbert <bruce.herbert@vu.edu.au>
2021-10-12 15:28:17 +1100

    iteration 1 changes

---
+++ Iteration 1/source/Node.gd
@@ -0,0 +1,40 @@
+extends Node
+
+# Version 1 - image zoom and pan 
+# Allows zoom in (up
+
+onready var paneA = $Viewports/ViewportContainer1/Viewport1/paneA
+onready var paneB = $Viewports/ViewportContainer2/Viewport2/paneB
+
+onready var viewport1 = $Viewports/ViewportContainer1/Viewport1
+onready var viewport2 = $Viewports/ViewportContainer2/Viewport2
+onready var camera1 = $Viewports/ViewportContainer1/Viewport1/Camera2D1
+onready var camera2 = $Viewports/ViewportContainer2/Viewport2/Camera2D2
+
+
+func _ready():
+	panel_active(true, false)
+	camera1.target = paneA.get_node("paneA")
+
+func _on_Button_Pane_A_active_pressed():
+	panel_active(true, false)
+
+func _on_Button_Pane_B_active_pressed():
+	panel_active(false, true)
+	
+func _input(event):
+	if event.is_action_pressed("paneA_active"):
+		panel_active(true, false)
+	if event.is_action_pressed("paneB_active"):
+		panel_active(false, true)
+
+func panel_active(paneA_active, paneB_active):
+	if paneA_active && !paneB_active:
+		paneA.isActive = true
+		paneB.isActive = false
+	if !paneA_active && paneB_active:
+		paneA.isActive = false
+		paneB.isActive = true
+	if !paneA_active && !paneB_active:
+		paneA.isActive = false
+		paneB.isActive = false
